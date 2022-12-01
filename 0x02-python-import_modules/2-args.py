bbe595f84318c50216d519194454a10b88816f02
Bsakwa <sakwabrian23@gmail.com>
2022-12-01 18:14:35 +0000

    Argv

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -14,4 +14,4 @@ if __name__ == "__main__":
     else:
         print("{} arguments:".format(argc))
     for i in range(argc):
-        print("{}:{}".format(i + 1, sys.argv[i + 1]))
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
