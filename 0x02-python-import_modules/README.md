0ea6a0b34761171460d69d6c685797533744a310
Bsakwa <sakwabrian23@gmail.com>
2022-12-01 15:42:20 +0000

    First Commit

---
+++ 0x02-python-import_modules/README.md
@@ -0,0 +1,1 @@
+We are now learning about Python Import Modules. This directory covers tasks related to the same.
---
+++ 0x02-python-import_modules/add_0.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def add(a, b):
+    """ My Addition Function
+
+    Args:
+        a: First Integer
+        b: Second Integer
+
+    Returns:
+        The value of a + b
+    """
+    return (a + b)
