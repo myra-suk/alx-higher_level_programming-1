168c6b22fc75176b889b9ccd9e151e01c0423435
Bsakwa <sakwabrian23@gmail.com>
2022-12-04 12:40:29 +0000

    Print Matrix

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def print_matrix_integer(matrix=[[]]):
+    if matrix:
+        for row in matrix:
+            for value in row:
+                if value <= row[0]:
+                    print("{:d}".format(value), end="")
+                else:
+                    print(" {:d}".format(value), end="")
+            print()
+    else:
+        print()
