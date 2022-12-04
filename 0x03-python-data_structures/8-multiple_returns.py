5ea7ca911c7f8e04227293dd24702560241d34a8
Bsakwa <sakwabrian23@gmail.com>
2022-12-04 13:09:55 +0000

    Multiple returns

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -5,7 +5,7 @@ def multiple_returns(sentence):
     Returns the length and the first letter of a string
 
     """
-    for i in range(len(sentence)):
-        if sentence == "":
-            return (0, None)
+    if len(sentence) == 0:
+        return (0, None)
+    else:
         return (len(sentence), sentence[0])
