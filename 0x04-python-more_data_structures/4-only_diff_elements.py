229b20f2cb14028fd3a4677d5afe1dcf162bf62e
Bsakwa <sakwabrian23@gmail.com>
2022-12-07 13:53:09 +0000

    Different elements in a set

---
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+# Author: Brian Sakwa
+
+def only_diff_elements(set_1, set_2):
+    """
+    Return different elements in a given pair of sets
+
+    """
+    return (set_1 ^ set_2)
