89046a8a4f5afb0326517e615f450d5f64f7cbde
Bsakwa <sakwabrian23@gmail.com>
2022-12-07 15:40:53 +0000

    Complex Delete

---
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+# Author: Brian Sakwa
+
+def complex_delete(a_dictionary, value):
+    if a_dictionary:
+        check = False
+        for k, v in a_dictionary.items():
+            if v == value:
+                check = k
+                if check:
+                    del a_dictionary[check]
+                    return (complex_delete(a_dictionary, value))
+        return (a_dictionary)
