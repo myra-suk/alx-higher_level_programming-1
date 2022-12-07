569002aabb4a925d4d23c4f14ee8c33f540d856c
Bsakwa <sakwabrian23@gmail.com>
2022-12-07 15:03:44 +0000

    Made it a 3 line file

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,10 +1,3 @@
 #!/usr/bin/python3
-
-# Author: Brian Sakwa
-
 def multiply_list_map(my_list=[], number=0):
-    """
-    Returns a list with all the values multiplied by a number
-
-    """
     return (list(map(lambda i: i * number, my_list)))
