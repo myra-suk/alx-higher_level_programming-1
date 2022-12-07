cd98c6023871063ff31a284e5da0182341eb61fc
Bsakwa <sakwabrian23@gmail.com>
2022-12-07 13:48:06 +0000

    Common elements in a set

---
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+# Author: Brian Sakwa
+
+def common_elements(set_1, set_2):
+    """
+    Return a set of common elements in a given pair of sets
+
+    """
+    return (set_1 & set_2)
