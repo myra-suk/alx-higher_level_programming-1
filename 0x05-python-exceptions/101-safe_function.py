f779c367b4bf22b16680a5098ffd6c87707377ec
Bsakwa <sakwabrian23@gmail.com>
2022-12-19 13:54:11 +0000

    Safe Function

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+# Author: Brian Sakwa
+import sys
+
+
+def safe_function(fct, *args):
+    """
+    Executes a function safely
+
+    """
+    try:
+        result = fct(*args)
+        return (result)
+    except:
+        print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
+        return (None)
