b516555c909848ff5a8ebf4b8f31653c247ffbe9
Bsakwa <sakwabrian23@gmail.com>
2022-12-19 13:14:05 +0000

    Divide the list

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+# Author: Brian Sakwa
+
+def list_division(my_list_1, my_list_2, list_length):
+    """
+    Divides element by element in two given lists
+
+    """
+    new_list = []
+    for i in range(0, list_length):
+        try:
+            div = my_list_1[i] / my_list_2[i]
+        except TypeError:
+            print("wrong type")
+            div = 0
+        except ZeroDivisionError:
+            print("divisioon by 0")
+            div = 0
+        except IndexError:
+            print("out of range")
+            div = 0
+        finally:
+            new_list.append(div)
+    return (new_list)
