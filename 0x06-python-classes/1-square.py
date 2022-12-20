714a6bab753ed8ad3d98e8f08615b9ef1a37f724
Bsakwa <sakwabrian23@gmail.com>
2022-12-20 15:05:14 +0000

    Second commit

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+# Author: Brian Sakwa
+
+"""Define a class Square"""
+
+
+class Square:
+    """Represent a class square"""
+
+    def __init__(self, size):
+        """Initialize a new instance.
+
+        Args:
+            size: The size of the new square.
+        """
+        self.__size = size
