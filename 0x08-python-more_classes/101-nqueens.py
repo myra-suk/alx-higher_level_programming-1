1352b3db2375bc7e1a4b6393b4108af6b0f1e5d9
Bsakwa <sakwabrian23@gmail.com>
2023-01-03 13:24:22 +0000

    Advanced Question trial

---
+++ 0x08-python-more_classes/101-nqueens.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+# Author: Brian Sakwa
+
+import sys
+
+
+def init_board(n):
+    """Initialize an `n`x`n` sized chessboard with 0's."""
+    board = []
+    [board.append([]) for i in range(n)]
+    [row.append(' ') for i in range(n) for row in board]
+    return (board)
