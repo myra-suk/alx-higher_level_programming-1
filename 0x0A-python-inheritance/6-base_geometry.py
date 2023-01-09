bb9088068b4d993a2ce86f07d73f17c9f4ab3321
Bsakwa <sakwabrian23@gmail.com>
2023-01-09 17:54:44 +0000

    Improve Geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+# Author: Brian Sakwa
+"""Defines a Geometry Module"""
+
+
+class BaseGeometry:
+    """Defines an empty class BaseGeometry"""
+
+    def area(self):
+        """Not implemented"""
+        raise Exception("area() is not implemented")
