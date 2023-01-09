196f8e038c4581b5a299d663aaf812b2f481fd59
Bsakwa <sakwabrian23@gmail.com>
2023-01-09 13:41:48 +0000

    Returns the lists of available attributes

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+# Author: Brian Sakwa
+"""Returns the list of available attributes and methods of an object"""
+
+
+def lookup(obj):
+    """Return a list of an objects available attributes"""
+    return (dir(object))
