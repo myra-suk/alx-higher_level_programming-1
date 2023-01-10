8a828f602d4ac9aa224b1d4b82e7483d38db1d61
Bsakwa <sakwabrian23@gmail.com>
2023-01-10 12:42:19 +0000

    Append

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,17 @@
+#!usr/bin/python3
+
+# Author: Brian Sakwa
+""" Defines a function that appends a string at the end ofa text file"""
+
+
+def append_write(filename="", text=""):
+    """ Appends a string
+
+    Args:
+        filename: The name of the file to append the string to
+        text: The string to append
+    Return:
+        The number of characters added
+    """
+    with open(filename, 'a', encoding="utf-8") as my_file:
+        return my_file.write(text)
