1aedd8c94165c031f882ff2745fd81ea23845590
Bsakwa <sakwabrian23@gmail.com>
2023-01-16 15:49:35 +0000

    Refactored

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -125,3 +125,13 @@ class Rectangle(Base):
                     self.x = (kwargs[kw])
                 if kw == "y":
                     self.y = (kwargs[kw])
+
+    def to_dictionary(self):
+        """Returns the dictionary representation of a Rectangle"""
+        return {
+            "id": self.id,
+            "width": self.width,
+            "height": self.height,
+            "x": self.x,
+            "y": self.y
+        }
