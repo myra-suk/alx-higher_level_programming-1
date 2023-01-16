c674a46435f9be618f9746cb20f8ae4b222cb82f
Bsakwa <sakwabrian23@gmail.com>
2023-01-16 16:47:17 +0000

    Added tests

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -1,3 +1,22 @@
 #!/usr/bin/python3
 
 # Author: Brian Sakwa
+"""unittest for Base class"""
+
+import unittest
+from models.base import Base
+
+
+class TestBase(unittest.TestCase):
+    """testing functions for Base class"""
+
+    def test_a_base_instantiation(self):
+        """test initialization of id if id None and if id int"""
+        b1 = Base()
+        self.assertEqual(b1.id, 1)
+        b2 = Base()
+        self.assertEqual(b2.id, 2)
+        b3 = Base(73)
+        self.assertEqual(b3.id, 73)
+        b4 = Base()
+        self.assertEqual(b4.id, 3)
