c30fe192460429ba9690c3b2a576ab1a4076f5ac
Bsakwa <sakwabrian23@gmail.com>
2023-01-16 16:13:11 +0000

    Refactored

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -50,7 +50,7 @@ class Base:
     @staticmethod
     def from_json_string(json_string):
         """ Returns the list of the Json String representation"""
-        if json_string is None:
+        if json_string is None or json_string == "[]":
             return []
         else:
             return json.loads(json_string)
