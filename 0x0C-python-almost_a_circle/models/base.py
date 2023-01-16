ab307a2a950c8b883f73b73d22f5f28f8ef34d09
Bsakwa <sakwabrian23@gmail.com>
2023-01-16 15:57:22 +0000

    Refactored

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -29,7 +29,7 @@ class Base:
     def to_json_string(list_dictionaries):
         """ Returns the JSON rep of list dictionaries"""
         if list_dictionaries is None or list_dictionaries == "[]":
-            return []
+            return "[]"
         return json.dumps(list_dictionaries)
 
     @classmethod
