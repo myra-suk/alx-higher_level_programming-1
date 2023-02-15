153e4af8e7dfd2ba89a8b4f66863329f8ee437e9
Bsakwa <sakwabrian23@gmail.com>
2023-02-15 19:21:48 +0000

    Only comedy

---
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,10 @@
+-- Lists all the shows that are comedy
+SELECT t.`title`
+FROM `tv_shows` AS t
+       INNER JOIN `tv_show_genres` AS s
+       ON t.`id` = s.`show_id`
+
+       INNER JOIN `tv_genres` AS g
+       ON g.`id` = s.`genre_id`
+       WHERE g.`name` = "Comedy"
+ORDER BY t.`title`;
