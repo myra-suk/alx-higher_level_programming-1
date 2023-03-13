ce73c6b3bda620bc353ff24e6a28ce7276411bbc
Bsakwa <sakwabrian23@gmail.com>
2023-03-13 15:57:42 +0000

    Display on different lines

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -2,8 +2,4 @@
 /*
  * Prints strings to the std output
  */
-const myvar = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-
-for (let i = 0; i < 3; i++) {
-  console.log(myvar[i]);
-}
+console.log('C is fun\nPython is cool\nJavaScript is amazing')
