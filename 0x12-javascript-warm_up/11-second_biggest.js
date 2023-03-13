ca4e1dfbed7dafd6447fc06ac04b27ccc990f949
Bsakwa <sakwabrian23@gmail.com>
2023-03-13 18:19:23 +0000

    Find the second largest

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,28 @@
+#!/usr/bin/node
+/*
+ * Searches for the biggest integer in the list of arguments
+ */
+const myArgs = process.argv.splice(2);
+if (myArgs.length === 0 || myArgs.length === 1) {
+  console.log(0);
+} else {
+  let i = 0;
+  let max = 0;
+  let result = 0;
+  if (Number(myArgs[0]) > Number(myArgs[1])) {
+    max = myArgs[0];
+    result = myArgs[1];
+  } else {
+    max = myArgs[1];
+    result = myArgs[0];
+  }
+  for (i = 2; i < myArgs.length; i++) {
+    if (Number(myArgs[i]) >= Number(max)) {
+      result = max;
+      max = myArgs[i];
+    } else if (Number(myArgs[i]) >= Number(result)) {
+      result = myArgs[i];
+    }
+  }
+  console.log(result);
+}
