4b1f82cd4b62f6b70789cb42bf48aa72103d7888
Bsakwa <sakwabrian23@gmail.com>
2023-03-13 15:53:38 +0000

    Display on different lines

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+/*
+ * Prints strings to the std output
+ */
+const myvar = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+for (let i = 0; i < 3; i++) {
+  console.log(myvar[i]);
+}
