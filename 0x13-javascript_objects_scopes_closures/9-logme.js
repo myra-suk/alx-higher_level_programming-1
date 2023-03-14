3b13c4b9d0a06b3db227a7e57a7c0a0e99d25208
Bsakwa <sakwabrian23@gmail.com>
2023-03-14 16:32:38 +0000

    Log me function

--- 0x13-javascript_objects_scopes_closures/9-logme.js
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -2,9 +2,9 @@
 /*
  * Prints the number of arguments and new argument value
  */
-let argvalue = 0;
 exports.logMe = function (item) {
-  const myprint = argvalue + ':' + item;
+  const myprint = argvalue + ': ' + item;
   console.log(myprint);
   argvalue += 1;
-};
+}
+let argvalue = 0;
