5938ccfcca0eeb154b8b6659549471a1ebaac69f
Bsakwa <sakwabrian23@gmail.com>
2023-03-14 15:19:35 +0000

    Add more functions for the class

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,37 @@
+#!/usr/bin/node
+/*
+ * Defines a Rectangle
+ */
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  // Method to print the Rectangle
+  print () {
+    let rect = '';
+    for (let i = 0; i < this.width; i++) {
+      rect += 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      console.log(rect);
+    }
+  }
+
+  // Method that exchanges the width and the height of the rectangle
+  rotate () {
+    const holder = this.height;
+    this.height = this.width;
+    this.width = holder;
+  }
+
+  // Method the width and height of the Rectangle by 2
+  double () {
+    this.height *= 2;
+    this.width *= 2;
+  }
+}
+module.exports = Rectangle;
